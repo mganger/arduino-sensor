e87e734ba140e26f46b73fa0f1f609b5e51f63d1
Michael Ganger <mganger747@gmail.com>
2015-06-18 22:54:01 -0400

    Added more sensors

--- example.cpp
+++ example.cpp
@@ -2,10 +2,18 @@
 #include <Accel.h>
 
 void setup()
-	Accel.init();
+	Accel      .init();
+	Gyro       .init();
+	Magnet     .init()
+	Pressure   .init();
+	Temperature.init();
 }
 
 void loop(){
-	Accel.read().print();
+	Accel      .read().print();
+	Gyro       .read().print();
+	Magnet     .read().print();
+	Pressure   .read().print();
+	Temperature.read().print();
 	delay(300);
 }
